01840e0f7d0c230a91294fa3f42ab24977ed8dfb
Bob den Otter <bob@twokings.nl>
2019-11-02 10:35:58 +0100

    🚀 Initial commit

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Resources')
+    ->exclude('Tests')
+    ->in($dir = __DIR__ . '/core')
+;
+
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create($dir)
+    ->add('master', 'master branch')
+;
+
+return new Sami($iterator, array(
+//    'theme'                => 'symfony',
+    'versions'             => $versions,
+    'title'                => 'Bolt 4 API',
+    'build_dir'            => __DIR__ . '/build/%version%',
+    'cache_dir'            => __DIR__ . '/cache/%version%',
+    'remote_repository'    => new GitHubRemoteRepository('bolt/core', dirname($dir)),
+    'default_opened_level' => 2,
+));
+
+
